e070ee571c894779a56ac7ed7669f7d0ec012197
Victor Rios Saucedo <36146644+Rotooo@users.noreply.github.com>
2024-07-26 21:15:30 -0600

    26-07-2024
    changes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import HomeBanner from '../containers/HomePage/Carousel';
+import Hola from '../components/Hola';
 
 export default function HomePage() {
   return (
     <div>
       <Navbar />
       <HomeBanner />
+      <Hola />
     </div>
   )
 }
